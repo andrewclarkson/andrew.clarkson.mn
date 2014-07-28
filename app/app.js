4abd3d02c9a94e47fc3a132a565f59659348c01a
Andrew Clarkson <andrewclarkson@live.com>
2014-07-27 23:39:59 -0500

    movin on up

---
+++ app/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var jade = require('jade');
+var pages = require('./routes/pages');
+//var admin = require('./routes/admin');
+
+var app = express();
+
+app.engine('jade', jade.__express);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+app.use('/', pages);
+//app.use('/admin', admin);
+
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/static'));
+
+module.exports = app;
+
---
+++ app/routes/pages.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('index');
+});
+
+router.get('/about', function(req, res) {
+    res.render('about');
+});
+
+router.get('/projects', function(req, res) {
+    res.render('projects');
+});
+
+module.exports = router;
