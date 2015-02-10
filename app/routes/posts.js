c0fd9e03a5e9aa62419a57bc4ab66ab821937348
Andrew Clarkson <andrew@clarkson.mn>
2015-02-09 23:06:30 -0600

    Add memcached

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -14,39 +14,63 @@ router.get('', function(req, res) {
     });
 });
 
+var cacheAndSend = function(req, res, key, value) { 
+    req.app.get('cache').set(key, value, 604800, function(err) {
+        if(err) {
+            console.log(err);
+        } 
+        res.status(200).send(value);
+    });
+};
+
 router.get('/:slug', function(req, res) {
   
-    var filename = path.join(req.app.get('posts'), req.params.slug);
-    fs.readFile(filename + '.md', {encoding: 'utf8'}, function(err, data) {
+    var type = req.accepts(['html', 'json']);
+
+    if(!type) {
+        return res.status(406).send("Cannot Create That Type"); 
+    }
+
+    var key = req.params.slug + '.' + type;
+    req.app.get('cache').get(key, function(err, data) {
         if(err) {
-            switch(err.errno) {
-                case 34:
-                    return res.status(404).send("Post not found");
-                    break;
-                default:
-                    return res.status(500).send("Internal Server Error");
-                    break;
+            console.log(err);
+        } else if (data) {
+            return res.status(200).type(type).send(data);
+        }
+    
+        var filename = path.join(req.app.get('posts'), req.params.slug);
+        fs.readFile(filename + '.md', {encoding: 'utf8'}, function(err, data) {
+            if (err) {
+                switch (err.errno) {
+                    case 34:
+                        return res.status(404).send("Post not found");
+                        break;
+                    default:
+                        return res.status(500).send("Internal Server Error");
+                        break;
+                }
             }
-        }
 
-        var parts = data.split('---', 3);
+            var parts = data.split('---', 3);
 
-        var post = yaml.eval(parts[1].trimLeft());
-        post.content = converter.makeHtml(parts[2]);
+            var post = yaml.eval(parts[1].trimLeft());
+            post.content = converter.makeHtml(parts[2]);
 
-        switch(req.accepts(['html', 'json'])) {
-            case 'html':
-                return res.render('post', post);
-                break;
-            case 'json':
-                return res.status(200).send(JSON.stringify(post));
-                break;
-            default:
-                return res.status(406).send("Cannot Create That Type"); 
-                break;
-        }
-
+            if (type == 'html') {
+                value = req.app.render('post', post, function(err, html) {
+                    if (err) { 
+                        throw err;
+                    }
+                    cacheAndSend(req, res, key, html);
+                });
+            } else {
+                cacheAndSend(req, res, key, JSON.stringify(post));
+            } 
+        });
     });
 });
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var app = require('./app');
 var path = require('path');
+var Memcached = require('memcached');
+var memcached = new Memcached('localhost:11211');
 
+app.set('cache', memcached);
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
