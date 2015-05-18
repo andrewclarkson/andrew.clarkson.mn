3091da40cccaae295d87aed0f9e1aac18228872c
Andrew Clarkson <andrew@clarkson.mn>
2015-05-17 23:47:58 -0500

    Add the basis of a GAE app

---
+++ server.py
@@ -0,0 +1,14 @@
+"""
+A simple wrapper around the flask app (app/__init__.py)
+adds the library directory "lib" into the system path so
+that python will recognize any libraries that are put there.
+"""
+
+import os
+import sys
+
+# add ./lib to the system path
+sys.path.insert(1, os.path.join(os.path.abspath('.'), 'lib'))
+
+# import the main WSGI app
+from app import app
